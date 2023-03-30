667e53fc7fb6e3674db508c59a176941b71ef57d
Azivast <olle.astre@tutamail.com>
2023-03-30 11:46:05 +0200

    Fix math

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -14,11 +14,11 @@ public class Collision : MonoBehaviour
 {
     // Input variables
     public Vector3 ballDirection = new Vector3(3, 0, 0);
-    public float ballRadius;
+    public float ballRadius = 1;
     public Vector3 ballPosition = new Vector3(10, 10 ,3);
 
     public Vector3 planeNormal = new Vector3(0.3f, 1, 0);
-    public float planeDistance = 1;
+    public Vector3 planeDistance = new Vector3(0, 1, 0);
     
     // Other Variables
     [NonSerialized] 
@@ -26,6 +26,12 @@ public class Collision : MonoBehaviour
 
     public Vector3 newBallPosition;
     public Vector3 impact;
+    public Vector3 aDir;
+    public float aLength;
+    public Vector3 aVec;
+    public Vector3 bDir;
+    public float bLength;
+    public Vector3 bVec;
 
 
     // Start is called before the first frame update
@@ -44,11 +50,14 @@ public class Collision : MonoBehaviour
         CalculateBounce();
         using (vectors.Begin())
         {
-            vectors.Draw(ballPosition, ballDirection, Color.red);
+            vectors.Draw(ballPosition, ballPosition+ballDirection, Color.red);
             vectors.Draw(Vector3.zero, planeNormal, Color.green);
             
             vectors.Draw(ballPosition, impact, Color.magenta);
-            vectors.Draw(impact, newBallPosition, Color.yellow);
+            vectors.Draw(impact, impact-aVec, Color.yellow);
+            vectors.Draw(impact-aVec, impact-aVec-aVec, Color.yellow);
+            vectors.Draw(impact-aVec-aVec, impact-aVec-aVec+bVec, Color.yellow);
+            vectors.Draw(impact, newBallPosition, Color.magenta);
         }
 
     }
@@ -62,27 +71,27 @@ public class Collision : MonoBehaviour
         //            ---___\
         //                  ---___plane
         
-        // â = -planeNormal
-        Vector3 aHat = -planeNormal;
         // ||a|| = ballPosition*nHat -planeDistance -ballRadius
-        float a = ballPosition.x * planeNormal.x + 
-                  ballPosition.y * planeNormal.y +
-                  ballPosition.z * planeNormal.z    // dot product
-                  
-                  - ballRadius
-                  - planeDistance;
-        
+        aLength =
+            ballPosition.x * planeNormal.x +
+            ballPosition.y * planeNormal.y +
+            ballPosition.z * planeNormal.z;    // dot product
+        aLength -= ballRadius - planeDistance.magnitude;
+        aDir = -planeNormal;
+        aVec = aLength * aDir;
+
         //  r / a = b / ballDirection.magnitude*r =>
         //  b = ballDirection.magnitude*(r^2) / a
-        float b = ballDirection.magnitude * ballRadius * ballRadius / a;
-        
-        // Move ball
-        Vector3 bVec = b * ballDirection;
-        Vector3 aVec = a * aHat;
+        bDir = ballDirection;
+        //Vector3 bDir = ballDirection * ballRadius * ballRadius / a;
+        bLength = aLength*aLength /
+                        (aVec.x * ballDirection.x +
+                        aVec.y * ballDirection.y +
+                        aVec.z * ballDirection.z);    // dot product
+        bVec = bDir * bLength;
 
         impact = ballPosition + bVec;
-        newBallPosition = impact - aVec - aVec + bVec;
-        
+        newBallPosition = impact - aVec - aVec + bVec; 
     }
 }
 
